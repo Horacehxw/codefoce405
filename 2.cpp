d6d42caa53f446c8a5f611d73f7e1424003f238c
Horacehxw <horacehxw@gmail.com>
2017-03-19 00:04:01 -0700

    fix bug and pass test: should use 'long long' instead of 'int'

--- 2.cpp
+++ 2.cpp
@@ -1,4 +1,4 @@
-#include <cstdio>
+// #include <cstdio>
 #include <iostream>
 #include <set>
 #include <cmath>
@@ -6,24 +6,24 @@ using namespace std;
 #define MAX_PEOPLE 150010
 
 typedef struct Mynode {
-    int size; // only the father maintains the size
-    int father;
+    long long size; // only the father maintains the size
+    long long father;
 }Mynode;
 
-class Mypair {
-    int a;
-    int b;
-    public:
-    Mypair(int a, int b) {
-        this->a = a;
-        this->b = b;
-    }
-};
+// class Mypair {
+//     long long a;
+//     long long b;
+//     public:
+//     Mypair(long long a, long long b) {
+//         this->a = a;
+//         this->b = b;
+//     }
+// };
 
 Mynode people[MAX_PEOPLE];
 
 //find the father + path compression
-int find(int x) {
+long long find(long long x) {
     if (people[x].father == x) {
         return x;
     }
@@ -31,9 +31,9 @@ int find(int x) {
 }
 
 //connect to set + restore size in the new father
-void connect(int a, int b) {
-    int f1 = find(a);
-    int f2 = find(b);
+void connect(long long a, long long b) {
+    long long f1 = find(a);
+    long long f2 = find(b);
     if (f1 == f2) {
         return;
     }
@@ -47,34 +47,34 @@ void connect(int a, int b) {
 }
 
 int main() {
-    int n, m;
-    scanf("%d %d", &n, &m);
-    for(int i = 0; i < MAX_PEOPLE; i++) { //initialize people
+    long long n, m;
+    cin >> n >> m;
+    for(long long i = 0; i <= n; i++) { //initialize people
         people[i].size = 1;
         people[i].father = i;
     }
-    set<pair<int, int> > pairs;
-    for (int i = 0; i < m; i++) {
-        int a, b;
-        scanf("%d %d", &a, &b);
+    set<pair<long long, long long> > pairs;
+    for (long long i = 0; i < m; i++) {
+        long long a, b;
+        cin >> a >> b;
         if (a != b) {
-            pairs.insert(*(new pair<int, int>(a, b)));
-            pairs.insert(*(new pair<int, int>(b, a)));
+            pairs.insert(*(new pair<long long, long long>(a, b)));
+            pairs.insert(*(new pair<long long, long long>(b, a)));
             connect(a, b);
         }
     }
-    int num_of_edge = pairs.size()/2;
-    int expect_edge = 0;
-    for (int i = 1; i < MAX_PEOPLE; i++) {
+    long long num_of_edge = pairs.size()/2;
+    long long expect_edge = 0;
+    for (long long i = 1; i <= n; i++) {
         if (find(i) == i) { // father of the set
-            int size_of_this = people[i].size;
+            long long size_of_this = people[i].size;
             expect_edge += (size_of_this-1)*size_of_this/2;
         }
     }
     if (expect_edge == num_of_edge) {
-        printf("YES");
+        cout << "YES";
     } else {
-        printf("NO");
+        cout << "NO";
     }
     return 0;
 }
